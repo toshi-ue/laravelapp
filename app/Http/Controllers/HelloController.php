25bfdce5eba26ebdee0351c75639d177dd56ae75
toshi-ue <t.ueno.dev+githubtoshiue@gmail.com>
2022-09-11 21:01:57 +0900

    HelloController.php に解説を追加

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -1,9 +1,14 @@
 <?php
 
+// 名前空間。
+// このディレクトリ配下にある
 namespace App\Http\Controllers;
 
+// use
+// 記述されているディレクトリのパッケージ(Illuminate\Http)のRequestを使えるようにする記述
 use Illuminate\Http\Request;
 
+// クラスの定義
 class HelloController extends Controller
 {
     //
