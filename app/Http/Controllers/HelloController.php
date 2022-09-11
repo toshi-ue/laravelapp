3890fdcaafe2a12b007485593fa83143c96b428a
toshi-ue <t.ueno.dev+githubtoshiue@gmail.com>
2022-09-11 22:49:43 +0900

    ルーティング、コントローラーを修正

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -7,32 +7,17 @@ namespace App\Http\Controllers;
 // use
 // 記述されているディレクトリのパッケージ(Illuminate\Http)のRequestを使えるようにする記述
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
-global $head, $style, $body, $end;
-$head = '<html><head>';
-$style = <<< EOF
-<style>
-body {font-size:16pt; color:#999;}
-h1 {font-size:100pt; text-align:right; color:#eee; margin: -40px 0px -50px 0px;}
-</style>
-EOF;
-$body = '</head><body>';
-$end = '</body></html>';
-
-function tag($tag, $txt)
-{
-    return "<{$tag}>" . $txt . "</{$tag}>";
-}
 
 // クラスの定義
 class HelloController extends Controller
 {
     // アクション
     //  コントローラに追加される処理のこと
-    public function index($id = 'noname', $pass = 'unknown')
+    public function index(Request $request, Response $response)
     {
-        return
-            <<< EOF
+        $html = <<< EOF
 <html>
 <head>
     <title>Hello/Index</title>
@@ -42,23 +27,16 @@ class HelloController extends Controller
 </style>
 <body>
     <h1>Index</h1>
-    <p>これはHelloコントローラーのindexアクションです</p>
-    <ul>
-        <li>ID: {$id}</li>
-        <li>PASS: {$pass}</li>
-    </ul>
-    <p><a href="/hello/other">go to other page</a></p>
+    <p>このページはHelloコントローラーのindexアクションです</p>
+    <h3>Request</h3>
+    <pre>{$request}</pre>
+    <h3>Response</h3>
+    <pre>{$response}</pre>
 </body>
 </html>
 EOF;
-    }
 
-    public function other()
-    {
-        global $head, $style, $body, $end;
-
-        $html = $head . tag('title', 'Hello/other') . $style . $body . tag('h1', 'other') . tag('p', 'this is other page') .  $end;
-
-        return $html;
+        $response->setContent($html);
+        return $response;
     }
 }
