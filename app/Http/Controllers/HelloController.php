6139896144c49bbd147ad171256aecdee06710ba
toshi-ue <t.ueno.dev+githubtoshiue@gmail.com>
2022-09-11 21:06:13 +0900

    HelloControllerにindexアクション(メソッド)を追加

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -11,5 +11,23 @@ use Illuminate\Http\Request;
 // クラスの定義
 class HelloController extends Controller
 {
-    //
+    // アクション
+    //  コントローラに追加される処理のこと
+    public function index()
+    {
+        return <<<EOF
+<html>
+<head>
+    <title>Hello</title>
+</head>
+<style>
+
+</style>
+<body>
+    <h1>Index</h1>
+    <p>これはHelloコントローラーのindexアクションです。</p>
+</body>
+</html>
+EOF;
+    }
 }
