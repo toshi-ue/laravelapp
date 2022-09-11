56e226f603565376e45cb7e720e690f8e4ade60d
toshi-ue <t.ueno.dev+githubtoshiue@gmail.com>
2022-09-12 05:47:03 +0900

    コントローラーでテンプレートを使用する

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -15,28 +15,8 @@ class HelloController extends Controller
 {
     // アクション
     //  コントローラに追加される処理のこと
-    public function index(Request $request, Response $response)
+    public function index()
     {
-        $html = <<< EOF
-<html>
-<head>
-    <title>Hello/Index</title>
-</head>
-<style>
-
-</style>
-<body>
-    <h1>Index</h1>
-    <p>このページはHelloコントローラーのindexアクションです</p>
-    <h3>Request</h3>
-    <pre>{$request}</pre>
-    <h3>Response</h3>
-    <pre>{$response}</pre>
-</body>
-</html>
-EOF;
-
-        $response->setContent($html);
-        return $response;
+        return view('hello.index');
     }
 }
