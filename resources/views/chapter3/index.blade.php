de3d3aebf768d2c5f6bef9f0b775cc8c5612fd65
toshi-ue <t.ueno.dev+githubtoshiue@gmail.com>
2022-09-12 15:04:43 +0900

    コンポーネントを使用して表示する

--- resources/views/chapter3/index.blade.php
+++ resources/views/chapter3/index.blade.php
@@ -17,10 +17,19 @@
 @endsection
 
 @section('content')
-    <p>ここが本文のコンテンツです。</p>
-    <p>必要なだけ記述できます。</p>
+    <p>ここが本文のコンテンツです</p>
+    <p>必要なだけ記述できます</p>
+    @component('components.message')
+        @slot('msg_title')
+        CAUTION!
+        @endslot
+
+        @slot('msg_content')
+        これはメッセージです
+        @endslot
+    @endcomponent
 @endsection
 
 @section('footer')
-copyright 2022 toshi-ue.
+    copyright 2022 toshi-ue.
 @endsection
