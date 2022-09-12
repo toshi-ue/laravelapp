baac8f659d735144cd2454ce91613fae3bbf0b95
toshi-ue <t.ueno.dev+githubtoshiue@gmail.com>
2022-09-12 10:55:24 +0900

    bladeテンプレートを使用する
    LaravelではBladeテンプレートがあるとそれが優先して読み込まれれる。

---
+++ resources/views/hello/index.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Hello/Index</title>
+    <style>
+        body {
+            font-size: 16pt;
+            color: #999;
+        }
+        h1 {
+            font-size: 100pt;
+            text-align: right;
+            color: #f6f6f6;
+            margin: -50px 0px -100px 0px;
+        }
+    </style>
+</head>
+<body>
+    <h1>Blade/Index</h1>
+    <p>{{ $msg }}</p>
+    <p>{{ date("Y年n月j日") }}</p>
+</body>
+
+</html>
